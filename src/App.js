a634e28847a7955513e4b1c6f656681023ad65f1
swalihtk <swalihmkd208@gmail.com>
2021-12-12 15:00:44 +0530

    port changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "antd/dist/antd.css";
 import { useEffect } from "react";
 
 // axios
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "http://127.0.0.1:3001/";
 axios.defaults.withCredentials = true;
 
 function App() {
